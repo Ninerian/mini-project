688f1b8f711f3b49044122ea383c2487907474a7
Daniel Große <daniel.grosse@staffbase.com>
2018-09-03 23:37:27 +0200

    feat(*): adds minus function

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,13 @@
 /* eslint-env jest, es6 */
 
-import add from '../src/index';
+import { add, minus } from '../src/index';
 
 describe('Test addition', () => {
   it('should add two numbers', () => {
     expect(add(1, 1)).toEqual(2);
+  });
+
+  it('should substract two numbers', () => {
+    expect(minus(2, 1)).toEqual(1);
   });
 });
